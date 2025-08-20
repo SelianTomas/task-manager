d3a3dac19f16d5a0c410711424e29f16279afc0e
SelianTomas <tomas.selian@gmail.com>
2025-08-20 14:06:56 +0200

    Task working

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.task_list, name='task_list'),
-    path('task/new/', views.task_create, name='task_create'),
-    path('task/<int:pk>/edit/', views.task_update, name='task_update'),
-    path('task/<int:pk>/delete/', views.task_delete, name='task_delete'),
+    path('new/', views.task_create, name='task_create'),
+    path('<int:pk>/edit/', views.task_update, name='task_update'),
+    path('<int:pk>/delete/', views.task_delete, name='task_delete'),
 ]
